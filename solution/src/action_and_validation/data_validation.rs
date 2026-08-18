b10c68854406b7be63eda292f45a2eec815d5195
# Golang <echo "Installation de Golang (ou dit GO).>
2026-08-18 12:28:30 +0200

    Corrections

--- solution/src/action_and_validation/data_validation.rs
+++ solution/src/action_and_validation/data_validation.rs
@@ -1,17 +1,45 @@
 pub fn validate_input(anfield: &str, piece: &str) -> (bool, String) {
 
     if anfield.is_empty(){
-        return false, String::from("anfield is empty.");
+        return (false, String::from("anfield is empty."));
     }
     if !anfield.chars().all(|c| matches!(c, '.' | '$' | 's' | '@' | 'a')) {
-        return false, String::from("anfield contains invalid characters.");
+        return (false, String::from("anfield contains invalid characters."));
     }
 
     if piece.is_empty(){
-        return false, String::from("piece is empty.");
+        return (false, String::from("piece is empty."));
     }
     if !piece.chars().all(|c| matches!(c, '.' | '#')) {
-        return false, String::from("piece contains invalid characters.");
+        return (false, String::from("piece contains invalid characters."));
     }
-    true, String::from("Input is valid.")
+    (true, String::from("Input is valid."))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn validate_input_test() {
+        let (is_valid, message) = validate_input("..$s@", ".#");
+        assert_eq!(is_valid, true);
+        assert_eq!(message, "Input is valid.");
+
+        let (is_valid, message) = validate_input("", ".#");
+        assert_eq!(is_valid, false);
+        assert_eq!(message, "anfield is empty.");
+
+        let (is_valid, message) = validate_input("..$s@", "");
+        assert_eq!(is_valid, false);
+        assert_eq!(message, "piece is empty.");
+
+        let (is_valid, message) = validate_input("..$s@x", ".#");
+        assert_eq!(is_valid, false);
+        assert_eq!(message, "anfield contains invalid characters.");
+
+        let (is_valid, message) = validate_input("..$s@", ".#x");
+        assert_eq!(is_valid, false);
+        assert_eq!(message, "piece contains invalid characters.");
+    }
 }
--- solution/src/main.rs
+++ solution/src/main.rs
@@ -2,16 +2,22 @@
 mod input;
 #[path = "input/input_data_formatting.rs"]
 mod input_data_formatting;
+#[path = "action_and_validation/data_validation.rs"]
+mod data_validation;
+
+use input::read_input;
+use input_data_formatting::format_input;
+use data_validation::validate_input;
 
 fn main() {
-    data_input := read_input_and_process();
-    anfield, piece := format_input(data_input);
-    is_input_ok, error_message := validate_input(anfield, piece);
-    
+    let data_input = read_input();
+    let (anfield, piece) = format_input(data_input);
+    let (is_input_ok, error_message) = validate_input(&anfield, &piece);
+
     if is_input_ok == false {
-        println!(error_message);
+        println!("{}",error_message);
         return;
     }
 
-    
+
 }
